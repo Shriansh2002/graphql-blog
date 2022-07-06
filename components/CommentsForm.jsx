dea4c9a98ec2558e67d0f31778dff1a51366bd48
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-07-06 21:53:39 +0530

    some more

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { submitComment } from '../services';
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
     const [error, setError] = useState(false);
     const [localStorage, setLocalStorage] = useState(null);
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
@@ -92,7 +93,10 @@ const CommentsForm = () => {
             </div>
             {error && <p className="text-xs text-red-500">All fields are mandatory</p>}
             <div className="mt-8">
-                <button type="button" onClick={handlePostSubmission} className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Post Comment</button>
+                <button
+                    type="button"
+                    onClick={handlePostSubmission}
+                    className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Post Comment</button>
                 {showSuccessMessage && <span className="text-xl float-right font-semibold mt-3 text-green-500">Comment submitted for review</span>}
             </div>
         </div>
