5da3151d6e8c429901ba92cb08164b001bf93baa
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-07-07 12:28:04 +0530

    featured post section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import { PostCard, PostWidget, Categories } from '../components';
+import FeaturedPosts from '../sections/FeaturedPosts';
 
 import { getPosts } from '../services';
 
@@ -16,6 +17,8 @@ const Home = ({ posts }) => {
 				<title>CMS Blog</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
+			<FeaturedPosts />
 
 			<div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
 				<div className="lg:col-span-8 col-span-1">
